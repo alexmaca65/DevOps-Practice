10073ae352db18f199a486d0c60e89300fdb4f7d
Alexandru Măcă <alexmaca09@gmail.com>
2025-07-15 22:47:27 +0300

    Refactored variables.tf, Created EC2 Bastion Host, created NAT Gateway, moved Internal Webseerver to Private Subnet

--- Practice-Upwork/Jobs/1-Setup-AWS-Infra-Terraform/ec2-instance.tf
+++ Practice-Upwork/Jobs/1-Setup-AWS-Infra-Terraform/ec2-instance.tf
@@ -1,60 +1,133 @@
 # EC2 Instance
 
 // data source ami
-resource "aws_instance" "ec2_instance" {
-  ami           = var.ec2_ami
-  instance_type = var.ec2_instance_type
-  user_data     = file("./ec2-user-data.sh")
 
-  vpc_security_group_ids = [aws_security_group.sg_ec2_instance.id]
+// EC2 Instances
+resource "aws_instance" "ec2_instance_bastion_host" {
+  ami           = var.ec2_instance_ami
+  instance_type = var.ec2_instance_type
+
+  vpc_security_group_ids = [aws_security_group.sg_ec2_instance_bastion_host.id]
   subnet_id              = aws_subnet.public_subnet_a.id
 
   tags = {
-    Name      = var.ec2_tag_name
+    Name      = var.ec2_instance_bastion_host_tag_name
     ManagedBy = var.managed_by
   }
 }
 
-resource "aws_security_group" "sg_ec2_instance" {
-  name        = var.sg_name
-  description = var.sg_description
+resource "aws_instance" "ec2_instance_internal_webserver" {
+  ami           = var.ec2_instance_ami
+  instance_type = var.ec2_instance_type
+  user_data     = file("./ec2-user-data.sh")
+
+  vpc_security_group_ids = [aws_security_group.sg_ec2_instance_internal_webserver.id]
+  subnet_id              = aws_subnet.private_subnet_a.id
+
+  tags = {
+    Name      = var.ec2_instance_internal_webserver_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Security Groups
+resource "aws_security_group" "sg_ec2_instance_bastion_host" {
+  name        = var.sg_bastion_host_name
+  description = var.sg_bastion_host_description
   vpc_id      = aws_vpc.custom_vpc.id
 
   tags = {
-    Name      = var.sg_tag_name
+    Name      = var.sg_bastion_host_tag_name
     ManagedBy = var.managed_by
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http_traffic" {
-  description       = var.sg_ingress_http_description
-  security_group_id = aws_security_group.sg_ec2_instance.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 80
-  to_port           = 80
+resource "aws_security_group" "sg_ec2_instance_internal_webserver" {
+  name        = var.sg_internal_webserver_name
+  description = var.sg_internal_webserver_description
+  vpc_id      = aws_vpc.custom_vpc.id
 
   tags = {
-    Name = var.sg_ingress_http_tag_name
+    Name      = var.sg_internal_webserver_tag_name
+    ManagedBy = var.managed_by
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_traffic" {
-  description       = var.sg_ingress_ssh_description
-  security_group_id = aws_security_group.sg_ec2_instance.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "tcp"
-  from_port         = 22
-  to_port           = 22
+// Security Group Ingress Rules
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_traffic_bastion_host" {
+  description       = var.sg_ingress_bastion_host_ssh_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
+
+  cidr_ipv4   = var.my_ip
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
 
   tags = {
-    Name = var.sg_ingress_ssh_tag_name
+    Name = var.sg_ingress_bastion_host_ssh_tag_name
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_trafic_internal_webserver" {
+  description       = var.sg_ingress_internal_webserver_ssh_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
+
+  referenced_security_group_id = aws_security_group.sg_ec2_instance_bastion_host.id
+
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+
+  tags = {
+    Name = var.sg_ingress_internal_webserver_ssh_tag_name
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_icmp_traffic_internal_webserver" {
+  description       = var.sg_ingress_internal_webserver_icmp_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
+
+  referenced_security_group_id = aws_security_group.sg_ec2_instance_bastion_host.id
+
+  ip_protocol = "icmp"
+  // -1 ==> All ports
+  from_port = -1
+  to_port   = -1
+
+  tags = {
+    Name = var.sg_ingress_internal_webserver_icmp_tag_name
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_traffic_internal_webserver" {
+  description       = var.sg_ingress_internal_webserver_http_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
+
+  referenced_security_group_id = aws_security_group.sg_ec2_instance_bastion_host.id
+
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+
+  tags = {
+    Name = var.sg_ingress_internal_webserver_http_tag_name
+  }
+}
+
+// Security Group Egress Rules
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
-  description       = "Allow all traffic"
-  security_group_id = aws_security_group.sg_ec2_instance.id
+  description       = var.sg_egress_general_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = -1 //semantically equivalent to all ports
+}
+
+// Elastic IP
+resource "aws_eip" "custom_eip" {
+  network_border_group = var.aws_region
+
+  tags = {
+    Name      = var.elastic_ip_tag_name
+    ManagedBy = var.managed_by
+  }
 }
