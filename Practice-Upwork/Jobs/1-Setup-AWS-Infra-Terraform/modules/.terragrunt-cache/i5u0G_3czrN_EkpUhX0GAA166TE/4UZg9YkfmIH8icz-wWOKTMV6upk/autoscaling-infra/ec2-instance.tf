8123d8fc021f22a430faf2e06da00d8e371d1be6
Alexandru Măcă <alexmaca09@gmail.com>
2025-07-22 11:57:59 +0300

    Upgraded to Terragrunt. Created main terragrunt.hcl and dev.yml. Initiated structure for Terraform Modules

---
+++ Practice-Upwork/Jobs/1-Setup-AWS-Infra-Terraform/modules/.terragrunt-cache/i5u0G_3czrN_EkpUhX0GAA166TE/4UZg9YkfmIH8icz-wWOKTMV6upk/autoscaling-infra/ec2-instance.tf
@@ -0,0 +1,156 @@
+# EC2 Instance
+
+// Locals
+locals {
+  security_group_ids_map = {
+    sg_bastion_host_id    = aws_security_group.sg_ec2_instance_bastion_host.id,
+    sg_internal_server_id = aws_security_group.sg_ec2_instance_internal_webserver.id,
+    sg_alb_id             = aws_security_group.sg_alb.id,
+    sg_lt_webserver_id    = aws_security_group.sg_lt_webserver.id
+  }
+}
+
+// EC2 Instances
+resource "aws_instance" "ec2_instance_bastion_host" {
+  ami = data.aws_ami.ami_amazon_linux_2023.id
+  // use t2.micro or t3.micro to be Free Eligible
+  instance_type = var.ec2_instance_type
+  user_data     = file("./user-data-scripts/bastion-host-ec2-user-data.sh")
+
+  vpc_security_group_ids = [aws_security_group.sg_ec2_instance_bastion_host.id]
+  subnet_id              = aws_subnet.public_subnet_a.id
+
+  tags = {
+    Name      = var.ec2_instance_bastion_host_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+resource "aws_instance" "ec2_instance_internal_webserver" {
+  ami           = data.aws_ami.ami_amazon_linux_2023.id
+  instance_type = var.ec2_instance_type
+  user_data     = file("./user-data-scripts/internal-webserver-ec2-user-data.sh")
+
+  vpc_security_group_ids = [aws_security_group.sg_ec2_instance_internal_webserver.id]
+  subnet_id              = aws_subnet.private_subnet_a.id
+
+  tags = {
+    Name      = var.ec2_instance_internal_webserver_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Security Groups
+resource "aws_security_group" "sg_ec2_instance_bastion_host" {
+  name        = var.sg_bastion_host_name
+  description = var.sg_bastion_host_description
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name      = var.sg_bastion_host_name
+    ManagedBy = var.managed_by
+  }
+}
+
+resource "aws_security_group" "sg_ec2_instance_internal_webserver" {
+  name        = var.sg_internal_webserver_name
+  description = var.sg_internal_webserver_description
+  vpc_id      = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name      = var.sg_internal_webserver_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Security Group Ingress Rules
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_traffic_bastion_host" {
+  description       = var.sg_ingress_bastion_host_ssh_description
+  security_group_id = aws_security_group.sg_ec2_instance_bastion_host.id
+
+  cidr_ipv4   = var.ssh_allowed_ip
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+
+  tags = {
+    Name = var.sg_ingress_bastion_host_ssh_tag_name
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_trafic_internal_webserver" {
+  description       = var.sg_ingress_internal_webserver_ssh_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
+
+  referenced_security_group_id = aws_security_group.sg_ec2_instance_bastion_host.id
+
+  ip_protocol = "tcp"
+  from_port   = 22
+  to_port     = 22
+
+  tags = {
+    Name = var.sg_ingress_internal_webserver_ssh_tag_name
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_icmp_traffic_internal_webserver" {
+  description       = var.sg_ingress_internal_webserver_icmp_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
+
+  referenced_security_group_id = aws_security_group.sg_ec2_instance_bastion_host.id
+
+  ip_protocol = "icmp"
+  // -1 ==> All ports
+  from_port = -1
+  to_port   = -1
+
+  tags = {
+    Name = var.sg_ingress_internal_webserver_icmp_tag_name
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_traffic_internal_webserver" {
+  description       = var.sg_ingress_internal_webserver_http_description
+  security_group_id = aws_security_group.sg_ec2_instance_internal_webserver.id
+
+  referenced_security_group_id = aws_security_group.sg_ec2_instance_bastion_host.id
+
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+
+  tags = {
+    Name = var.sg_ingress_internal_webserver_http_tag_name
+  }
+}
+
+// Security Group Egress Rules
+resource "aws_vpc_security_group_egress_rule" "outbound_allow_all_traffic_general" {
+  depends_on = [
+    aws_security_group.sg_ec2_instance_bastion_host,
+    aws_security_group.sg_ec2_instance_internal_webserver,
+    aws_security_group.sg_alb,
+    aws_security_group.sg_lt_webserver
+  ]
+
+  for_each = local.security_group_ids_map
+
+  description       = var.sg_egress_general_description
+  security_group_id = each.value
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = -1 //semantically equivalent to all ports
+
+  tags = {
+    Name = var.sg_egress_general_description
+  }
+}
+
+// Elastic IP
+resource "aws_eip" "custom_eip" {
+  network_border_group = var.aws_region
+
+  tags = {
+    Name      = var.elastic_ip_tag_name
+    ManagedBy = var.managed_by
+  }
+}
---
+++ Practice-Upwork/Jobs/1-Setup-AWS-Infra-Terraform/modules/.terragrunt-cache/i5u0G_3czrN_EkpUhX0GAA166TE/4UZg9YkfmIH8icz-wWOKTMV6upk/vpc.tf
@@ -0,0 +1,190 @@
+# VPC and Networking
+
+// VPC
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name      = var.vpc_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Public Subnets
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  cidr_block = "10.1.0.0/24"
+
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = var.public_subnet_a_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  cidr_block = "10.1.1.0/24"
+
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name      = var.public_subnet_b_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+//Private Subnets
+resource "aws_subnet" "private_subnet_a" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  cidr_block = "10.1.16.0/20"
+
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name      = var.private_subnet_a_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+  vpc_id     = aws_vpc.custom_vpc.id
+  cidr_block = "10.1.32.0/20"
+
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name      = var.private_subnet_b_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    // route to outside to igw
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.custom_igw.id
+  }
+
+  route {
+    // route to local
+    cidr_block = aws_vpc.custom_vpc.cidr_block
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name      = var.public_route_table_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Public Route Table Association with Public Subnets
+resource "aws_route_table_association" "public_rt_association_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_rt_association_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+// Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    // route to nat gw
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.custom_ngw.id
+  }
+
+  route {
+    // route to local
+    cidr_block = aws_vpc.custom_vpc.cidr_block
+    gateway_id = "local"
+  }
+
+  tags = {
+    Name      = var.private_route_table_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Private Route Table Association with Private Subnets
+resource "aws_route_table_association" "private_rt_association_a" {
+  subnet_id      = aws_subnet.private_subnet_a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_rt_association_b" {
+  subnet_id      = aws_subnet.private_subnet_b.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+// Network ACL
+resource "aws_network_acl" "name" {
+  vpc_id = aws_vpc.custom_vpc.id
+  subnet_ids = [
+    aws_subnet.public_subnet_a.id,
+    aws_subnet.public_subnet_b.id,
+    aws_subnet.private_subnet_a.id,
+    aws_subnet.private_subnet_b.id
+  ]
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  tags = {
+    Name      = var.network_acl_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// Internet Gateway
+resource "aws_internet_gateway" "custom_igw" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  tags = {
+    Name      = var.internet_gateway_tag_name
+    ManagedBy = var.managed_by
+  }
+}
+
+// NAT Gateway
+resource "aws_nat_gateway" "custom_ngw" {
+  depends_on = [
+    aws_internet_gateway.custom_igw,
+    aws_eip.custom_eip
+  ]
+
+  connectivity_type = "public"
+  subnet_id         = aws_subnet.public_subnet_a.id
+  allocation_id     = aws_eip.custom_eip.id
+
+  tags = {
+    Name      = var.nat_gateway_tag_name
+    ManagedBy = var.managed_by
+  }
+}
